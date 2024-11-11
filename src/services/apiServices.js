52e0541aadbbf6052b11b8f7fa718f8a957a3094
Kidanny Roman <97367805+TechKidJr@users.noreply.github.com>
2024-11-11 08:15:32 -0500

    Create apiServices.js

---
+++ src/services/apiServices.js
@@ -0,0 +1,19 @@
+// src/services/apiService.js
+
+const API_URL = 'http://localhost:5000';
+
+export async function shortenURL(longUrl) {
+  const response = await fetch(`${API_URL}/shorten`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ longUrl }),
+  });
+  return response.json();
+}
+
+export async function getShortenedURLs() {
+  const response = await fetch(`${API_URL}/urls`);
+  return response.json();
+}
