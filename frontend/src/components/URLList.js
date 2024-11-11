ed4efd3f0d651d7062ec781c2b39326c3cd1401d
Kidanny Roman <97367805+TechKidJr@users.noreply.github.com>
2024-11-11 13:10:09 -0500

    Create URLList.js
    Idk

---
+++ frontend/src/components/URLList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function URLList({ urls }) {
+  return (
+    <ul>
+      {urls.map((url) => (
+        <li key={url.shortUrl}>
+          <a href={url.shortUrl} target="_blank" rel="noopener noreferrer">
+            {url.shortUrl}
+          </a> - {url.longUrl}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default URLList;
